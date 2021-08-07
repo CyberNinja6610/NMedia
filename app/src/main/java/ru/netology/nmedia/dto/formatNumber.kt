821557167d0db5f5bcb86f62efe8f5487b52e16c
SpaceCadet <spacecadet2702@gmail.com>
2021-08-08 00:30:34 +0500

    add event listeners for likes and share

---
+++ app/src/main/java/ru/netology/nmedia/dto/formatNumber.kt
@@ -0,0 +1,16 @@
+package ru.netology.nmedia.dto
+
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.*
+
+fun Int.formatToString(): String {
+    val symbols = DecimalFormatSymbols(Locale.getDefault());
+    symbols.decimalSeparator = '.';//simple space
+    return when {
+        this >= 1_000_000 -> "${DecimalFormat("#.#", symbols).format(this / 1_000_000.0)}M"
+        this >= 10_000 -> "${DecimalFormat("#", symbols).format(this / 1_000.0)}K"
+        this >= 1_100 -> "${DecimalFormat("#.#", symbols).format(this / 1_000.0)}K"
+        else -> this.toString()
+    }
+}
